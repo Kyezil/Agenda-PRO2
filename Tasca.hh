7b929e376a94675b50075d05005d900cf0592bf2
Louis Clergue <louisclergue@gmail.com>
2015-04-30 23:21:27 +0200

    Fer que compili, afegir constructors i treure string&

--- Tasca.hh
+++ Tasca.hh
@@ -6,29 +6,42 @@
 #define _TASCA_HH_
 #include <string>
 #include <set>
+using namespace std;
 
 class Tasca {
     private:
-        string titol;;
-        /// \invariant \b etiquetes no conté etiquetes repetides
+        string titol;
+        /// \invariant \b etiquetes no conté elements repetits
         set<string> etiquetes; 
     public:
+        /** \brief Constructor per defecte
+         *  \pre true
+         *  \post el p.i és una tasca sense etiquetes
+         */
+        Tasca();
+
+        /** \brief Constructor de tasca amb titol
+         *  \pre true
+         *  \post el p.i és una tasca de \b titol = \e titol
+         */
+        Tasca(string titol);
+
         /** \brief Modifica el títol de la tasca
          *  \pre true
-         *  \post el títol del p.i és \e titol
+         *  \post el \b titol del p.i és \e titol
          */
-        modificar_titol(string& titol);
+        void modificar_titol(string titol);
 
         /** \brief Afegeix una etiqueta a la tasca
          *  \pre true
          *  \post el p.i té una etiqueta \e etiq
          */
-        afegir_etiqueta(string& etiq);
+        void afegir_etiqueta(string etiq);
 
         /** \brief Esborrar una etiqueta de la tasca
          *  \pre true
          *  \post el p.i no té com a etiqueta \e etiq
          */
-        esborrar_etiqueta(string& etiq);
-}
+        void esborrar_etiqueta(string etiq);
+};
 #endif
