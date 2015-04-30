6f1a06291264b0d84e7093669e048448e7990baf
Louis Clergue <louisclergue@gmail.com>
2015-04-30 23:34:30 +0200

    afegir tipus parametres (doxygen)

--- Tasca.hh
+++ Tasca.hh
@@ -9,6 +9,9 @@
 using namespace std;
 
 class Tasca {
+    /** \class Tasca
+     *  \brief Representa una tasca amb etiquetes associades
+     */
     private:
         string titol;
         /// \invariant \b etiquetes no conté elements repetits
@@ -21,24 +24,28 @@ class Tasca {
         Tasca();
 
         /** \brief Constructor de tasca amb titol
+         *  \param [in] titol Títol de la tasca a crear
          *  \pre true
          *  \post el p.i és una tasca de \b titol = \e titol
          */
         Tasca(string titol);
 
         /** \brief Modifica el títol de la tasca
+         *  \param [in] titol Nou títol de la tasca
          *  \pre true
          *  \post el \b titol del p.i és \e titol
          */
         void modificar_titol(string titol);
 
         /** \brief Afegeix una etiqueta a la tasca
+         *  \param [in] etiq Etiqueta a afegir
          *  \pre true
          *  \post el p.i té una etiqueta \e etiq
          */
         void afegir_etiqueta(string etiq);
 
         /** \brief Esborrar una etiqueta de la tasca
+         *  \param [in] etiq Etiqueta a esborrar
          *  \pre true
          *  \post el p.i no té com a etiqueta \e etiq
          */
