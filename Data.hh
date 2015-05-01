aca45443a1c08c5a14b01e3cbff4ff9d6572da64
Louis Clergue <louisclergue@gmail.com>
2015-05-01 09:58:49 +0200

    simplificar data
    Conversió de classe en struct (guany de les constructores), const a operator<  (he buscar i es pot)
    Similar a Data de http://www.cs.upc.edu/~pro2/data/uploads/diccionaris.pdf pàgina 6

--- Data.hh
+++ Data.hh
@@ -1,48 +1,25 @@
 /** \file Data.hh
-    \brief Especificació de la classe Data
+    \brief Especificació de Data
 */
-
 #ifndef _DATA_HH_
 #define _DATA_HH_
-
-/** \class Data
-    \brief Representa una data amb els atributs any, mes, dia, hora i minut
+/** \struct Data
+    \brief Representa una data dia:mes:any hora:minut
 */
-class Data
-{
-protected:
-  
-  unsigned int any;   /// \invariant \f$ any \le 99 \f$
-  unsigned int mes;   /// \invariant \f$ 1 \le mes \le 12 \f$
-  unsigned int dia;   /// \invariant \f$ 1 \le dia \le 31 \f$
-  unsigned int hora;  /// \invariant \f$ hora < 24 \f$
-  unsigned int minut; /// \invariant \f$ minut < 60 \f$
-  
-public:
-  
-  //Constructores
-  
-  /** \brief Creadora per defecte.
-      \pre Cert
-      \post El resultat és una data per defecte.
+struct Data {
+    unsigned int any;   /// \invariant \f$ \textbf{any} \le 99 \f$
+    unsigned int mes;   /// \invariant \f$ 1 \le \textbf{mes} \le 12 \f$
+    unsigned int dia;   /// \invariant \f$ 1 \le \textbf{dia} \le 31 \f$
+    unsigned int hora;  /// \invariant \f$ \textbf{hora} < 24 \f$
+    unsigned int minut; /// \invariant \f$ \textbf{minut} < 60 \f$
+    
+    /** \brief Indica ordre entre dates (<).
+     *  \param[in] a lhs data
+     *  \param[in] b rhs data
+     *  \return (a < b)
+     *  \pre Cert
+     *  \post El resultat indica si la primera data és anterior a la segona.
    */
-  Data();
-  
-  /** \brief Creadora amb valors inicials.
-      \pre \f$ \textit{any} \le 99, 1 \le \textit{mes} \le 12, 
-      1 \le \textit{dia} \le 31, \textit{hora} < 24,
-      \textit{minut} < 60 \f$
-      \post El p.i. representa la data dels paràmetres.
-   */
-  Data(int any, int mes, int dia, int hora, int minut);  
-  
-  // Consultores
-  
-  /** \brief Indica ordre entre dates (<).
-      \pre Cert
-      \post El resultat indica si la primera data és anterior a la segona.
-   */
-  bool operator< (Data &a, Data &b);
+   bool operator<(const Data &a, const Data &b) const;
 };
-
 #endif
