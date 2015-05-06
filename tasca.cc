f769c32b322219ce0b9a95ae3548e380f3d0b182
Ignacio Rasche <nacho.rasche@gmail.com>
2015-05-06 15:47:50 +0200

    Create tasca.cc

---
+++ tasca.cc
@@ -0,0 +1,12 @@
+#include "tasca.hh" 
+Tasca::Tasca(string titol): titol(titol){
+} 
+void Tasca::modificar_titol(string titol){
+  this->titol=titol; 
+}
+void Tasca::afegir_etiqueta(string etiq){
+ etiquetes.insert(etiq); 
+}
+void Tasca::esborrar_etiqueta(string etiq){
+  etiquetes.erase(etiq);
+}
