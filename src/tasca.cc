bcc5226c2c087e3676e984815ecb03a94e35a011
IrisBC <irisbc144@gmail.com>
2015-05-13 16:03:58 +0200

    Update tasca.cc

--- src/tasca.cc
+++ src/tasca.cc
@@ -2,7 +2,7 @@
  *  \file tasca.cc
  *  \brief Implementació Tasca
  */
-#include "tasca.hh" 
+#include "tasca.hh"
 
 Tasca::Tasca(string titol): title_(titol) {}
 
@@ -20,4 +20,18 @@ void Tasca::del_etiqueta(string etiq) {
 
 void Tasca::del_etiquetes() {
     tags_.clear();
+}
+
+void Tasca::print_titol(const Tasca &t, ostream &out) {
+    out << t.title_;
+}
+
+void Tasca::print_etiquetes(const Tasca &t, ostream &out) {
+    set<string>::const_iterator it = t.tags_.begin();
+    if (it != t.tags_.end()) out << '#' << *it;
+    ++it;
+    while (it != t.tags_.end()) {
+        out << " #" << *it; // Falta veure si cal #
+        ++it;
+    }
 }
