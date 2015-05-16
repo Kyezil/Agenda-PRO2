ef12c3ac2cc1b94cdcf411822e9f381501eb8d07
Louis Clergue <louisclergue@gmail.com>
2015-05-16 21:51:32 +0200

    Millorar test agenda

--- src/test_agenda.cc
+++ src/test_agenda.cc
@@ -10,5 +10,12 @@ int main() {
     Tasca t ("hola");
     t.add_etiqueta("hi");
     ag.add_tasca({{3,2,1},{7,5}}, t);
+    Tasca t1 ("sopar");
+    t1.add_etiqueta("miam");
+    t1.add_etiqueta("arròs");
+    ag.add_tasca({{2,2,1},{21,0}}, t1);
+    cout << "PASSAT\n";
     ag.passat();
+    cout << "FUTUR\n";
+    ag.consulta();
 }
