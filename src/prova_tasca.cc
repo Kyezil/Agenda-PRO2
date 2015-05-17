62f977cfebe5e3802b8cfb11fd1ecff5c7776ec5
Louis Clergue <louisclergue@gmail.com>
2015-05-17 09:56:15 +0200

    tasca: afegir prova de conversió implícita des de string

--- src/prova_tasca.cc
+++ src/prova_tasca.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include "tasca.hh"
 using namespace std;
 
@@ -6,22 +7,32 @@ int main() {
     Tasca t ("la meva tasca");
     Tasca::print_titol(t, cout);
     cout << endl;
+
     t.set_titol("canvi de nom");
     Tasca::print_titol(t, cout);
     cout << endl;
+
     Tasca::print_etiquetes(t, cout);
     cout << endl;
+
     t.add_etiqueta("arbre");
     t.add_etiqueta("poma");
     Tasca::print_etiquetes(t, cout);
     cout << endl;
+
     t.del_etiqueta("arbre");
     Tasca::print_etiquetes(t, cout);
     cout << endl;
+
     t.add_etiqueta("arbre");
     Tasca::print_etiquetes(t, cout);
     cout << endl;
+
     t.del_etiquetes();
     Tasca::print_etiquetes(t, cout);
+    cout << endl;
+
+    Tasca t2 = string("conversió implícita");
+    Tasca::print_titol(t2, cout);
     cout << endl;
 }
